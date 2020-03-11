c3dc49b85ac0b68273c690aa89db3e09130cd385
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-11 15:58:02 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -5,7 +5,7 @@ str=$(docker port test-mysql)
 IFS=':'
 read -ra ADDR <<< "$str"
 docker_mysql_port=${ADDR[1]}
-mysql -P $docker_mysql_port --protocol=tcp -u root -pHGKO$.xu1234 -Bse "drop database if exists db5;create database db5;"
+mysql -P $docker_mysql_port --protocol=tcp -u root -pHGKO$.xu1234 -Bse "drop database if exists db5;create database db5;use db5;"
 
 
 input="/var/lib/jenkins/workspace/03-11-V1/logfile.txt"
