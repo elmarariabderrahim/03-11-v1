3410956b74dc66103bda3b3939df4d0fd8624373
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-12 10:17:22 +0100

    Create sql1.sql

---
+++ sql_scripts/sql1.sql
@@ -0,0 +1,12 @@
+insert into departements values ('1','IT');
+insert into departements values ('2','LO');
+insert into departements values ('3','EL');
+insert into departements values ('4','PH');
+insert into departements values ('5','RH');
+insert into departements values ('6','RS');
+insert into departements values ('7','SE');
+
+
+insert into employees values (1,'2020-01-01','med','lhkim','M','2020-01-01');
+insert into employees values (2,'2020-01-01','med','lhkim','M','2020-01-01');
+insert into employees values (3,'2020-01-01','med','lhkim','M','2020-01-01');
