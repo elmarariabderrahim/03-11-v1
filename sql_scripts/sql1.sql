ebcec655915b503cad17593cf0b7ca44b95aa19a
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-12 10:24:50 +0100

    Update sql1.sql

--- sql_scripts/sql1.sql
+++ sql_scripts/sql1.sql
@@ -1,11 +1,11 @@
 use db5;
-insert into departements values ('1','IT');
-insert into departements values ('2','LO');
-insert into departements values ('3','EL');
-insert into departements values ('4','PH');
-insert into departements values ('5','RH');
-insert into departements values ('6','RS');
-insert into departements values ('7','SE');
+insert into departments values ('1','IT');
+insert into departments values ('2','LO');
+insert into departments values ('3','EL');
+insert into departments values ('4','PH');
+insert into departments values ('5','RH');
+insert into departments values ('6','RS');
+insert into departments values ('7','SE');
 
 
 insert into employees values (1,'2020-01-01','med','lhkim','M','2020-01-01');
